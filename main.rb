8a38b6426e1673e1380f131d477df8303477387b
Jon Wexler <jwexman@gmail.com>
2016-02-23 01:22:59 -0500

    added json and rake

--- main.rb
+++ main.rb
@@ -1,19 +1,33 @@
 require './config'
 
+def current_user
+  session[:user_id].nil? ? nil : User.find(session[:user_id])
+end
 
-# def current_user
-#   session[:user_id].nil? ? nil : User.find(session[:user_id])
-# end
-
-# def resp(code, result = {})
-#   {status: code, result: result}.to_json
-# end
+def resp(code, result = {})
+  {status: code, result: result}.to_json
+end
 
 get '/' do 
 
     "Ok"
 end
 
-# namespace '/api' do
+namespace '/api' do
+  namespace "/sessions" do
+    get "/" do
+      resp(200, current_user.as_json)
+  end
 
-# end
+  post "/new" do
+      @user = User.authenticate(params[:email], params[:password])
+      @user ? session[:user_id] = @user.id : nil
+      resp(200, current_user.as_json)
+  end
+
+  get "/destroy" do
+      session[:user_id] = nil
+      resp(200, {success: "User logged out successfully."})
+  end
+end
+end
