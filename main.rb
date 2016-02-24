bd2b65bf87da20acfe17402978046846ec81814a
Jon Wexler <jwexman@gmail.com>
2016-02-23 23:49:42 -0500

    basic user show and post

--- main.rb
+++ main.rb
@@ -1,33 +1,67 @@
 require './config'
 
-def current_user
-  session[:user_id].nil? ? nil : User.find(session[:user_id])
-end
+# def current_user
+#   session[:user_id].nil? ? nil : User.find(session[:user_id])
+# end
 
 def resp(code, result = {})
   {status: code, result: result}.to_json
 end
 
-get '/' do 
+def parsed_params
+    puts JSON.parse(params, :symbolize_names => true)
+end
 
+get '/' do 
     "Ok"
 end
 
-namespace '/api' do
-  namespace "/sessions" do
-    get "/" do
-      resp(200, current_user.as_json)
-  end
-
-  post "/new" do
-      @user = User.authenticate(params[:email], params[:password])
-      @user ? session[:user_id] = @user.id : nil
-      resp(200, current_user.as_json)
-  end
-
-  get "/destroy" do
-      session[:user_id] = nil
-      resp(200, {success: "User logged out successfully."})
-  end
+get '/users/all' do 
+    @users = User.all
+    @users.first.to_json
 end
+
+get '/users/:id' do 
+    @user = User.find(params[:id])
+    @user.to_json
+end
+
+namespace '/api' do
+    # namespace "/sessions" do
+    # # get "/" do
+    # #   session[:user_id].as_json
+    # # end
+
+    #     post "/new" do
+    #          puts "SIGNING IN"
+    #          # jdata = JSON.parse(params[:data],:symbolize_names => true) 
+    #          jons = JSON.parse(params[:params], :symbolize_names => true)
+    #          puts "params are #{jons.inspect}"
+
+    #          @user = User.authenticate(jons[:email], jons[:password])
+    #          @user ? session[:user_id] = @user.id : nil
+    #          @user.to_json 
+    #     end  
+
+    #     get "/destroy" do
+    #       session[:user_id] = nil
+    #       {success: "User logged out successfully."}.to_json
+    #     end
+    # end
+
+    namespace "/users" do 
+        post '/' do 
+            puts "Params Are: #{params}"
+            # params = parsed_params[:user]
+            # puts "EMAIL"
+            # puts parsed_params[:email]
+            # @user = User.where(email: params[:email]).first
+            # puts @user if @user != nil
+            # parsed_params.to_json
+            "PARAMS"
+        end
+        get '/' do 
+            User.all.to_json
+        end
+    end
 end
---
+++ user_client.rb
@@ -0,0 +1,39 @@
+require 'json' 
+require 'rest-client' 
+
+class UserClient 
+  attr_accessor :first_name, :last_name, :email, :password 
+
+  def initialize options={} 
+    @first_name = options[:first_name] 
+    @last_name = options[:last_name] 
+    @email = options[:email] 
+    @password = options[:password] 
+  end 
+
+  # def sign_in 
+  #   response = RestClient.post 'http://localhost:9292/api/sessions/new', :data => {email: @email, password: @password}.to_json, :accept => :json 
+  #   puts response
+  # end 
+
+  # def sign_out 
+  #   response = RestClient.get 'http://localhost:9292/api/sessions/destroy', :data => {:email => @email}.to_json    
+  #   puts response 
+  # end 
+
+  # def sessions 
+  #   response = RestClient.get 'http://localhost:9292/api/sessions/', :data =>{}.to_json, :accept => :json 
+  #   puts response 
+  # end 
+  def show_all
+    response = RestClient.get 'http://localhost:9292/api/users/', :user =>{}, :accept => :json 
+      puts response 
+  end
+    def save 
+      response = RestClient.post 'http://localhost:9292/api/users/', :user =>{          first_name: @first_name,
+        last_name: @last_name,
+        password: @password,
+        email: @email}.to_json, :accept => :json 
+        puts response 
+      end 
+    end
