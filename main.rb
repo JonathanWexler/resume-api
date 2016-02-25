7bdf7a96e66b0df9628a63f7d9777aee76a8801b
Jon Wexler <jwexman@gmail.com>
2016-02-25 18:59:57 -0500

    posting user works

--- main.rb
+++ main.rb
@@ -27,41 +27,36 @@ get '/users/:id' do
 end
 
 namespace '/api' do
-    # namespace "/sessions" do
-    # # get "/" do
-    # #   session[:user_id].as_json
-    # # end
-
-    #     post "/new" do
-    #          puts "SIGNING IN"
-    #          # jdata = JSON.parse(params[:data],:symbolize_names => true) 
-    #          jons = JSON.parse(params[:params], :symbolize_names => true)
-    #          puts "params are #{jons.inspect}"
-
-    #          @user = User.authenticate(jons[:email], jons[:password])
-    #          @user ? session[:user_id] = @user.id : nil
-    #          @user.to_json 
-    #     end  
-
-    #     get "/destroy" do
-    #       session[:user_id] = nil
-    #       {success: "User logged out successfully."}.to_json
-    #     end
-    # end
-
     namespace "/users" do 
         post '/' do 
             puts "Params Are: #{params}"
+            puts "USER IS: #{user_params}"
+            u = User.new(user_params)
+            if u.save
+                "YEP"
+            else
+                "NOPe"
+            end
             # params = parsed_params[:user]
             # puts "EMAIL"
             # puts parsed_params[:email]
             # @user = User.where(email: params[:email]).first
             # puts @user if @user != nil
             # parsed_params.to_json
-            "PARAMS"
+            
         end
         get '/' do 
             User.all.to_json
         end
     end
+end
+
+private
+
+def user_params
+    u = params[:user]
+    puts u = JSON.parse(u)
+
+    puts h =  {first_name: u['first_name'], last_name: u['last_name'], email: u['email'], password: u['password']}
+    h
 end
